e478bbe8e86319dd59a7618b6176372e92ca9d16
Tien Tai Nguyen <nguyentientai.17122004@gmail.com>
2026-04-05 11:32:36 +0700

    Xây dựng navbar và component menu

---
+++ BDSKhanhHoa/Components/NavbarMenuViewComponent.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BDSKhanhHoa.Data;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace BDSKhanhHoa.Components
+{
+    public class NavbarMenuViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+
+        public NavbarMenuViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var allTypes = await _context.PropertyTypes.ToListAsync();
+
+            // ID 1: Mua bán, ID 2: Cho thuê (như logic database bạn đã tạo)
+            var model = new NavbarMenuViewModel
+            {
+                BuyCategories = allTypes.Where(t => t.ParentID == 1).ToList(),
+                RentCategories = allTypes.Where(t => t.ParentID == 2).ToList()
+            };
+
+            return View(model);
+        }
+    }
+
+    public class NavbarMenuViewModel
+    {
+        public List<BDSKhanhHoa.Models.PropertyType> BuyCategories { get; set; }
+        public List<BDSKhanhHoa.Models.PropertyType> RentCategories { get; set; }
+    }
+}
