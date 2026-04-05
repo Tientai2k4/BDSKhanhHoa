2e965637c25671338ffcb7f0365d6dca9957971f
Tien Tai Nguyen <nguyentientai.17122004@gmail.com>
2026-04-05 12:09:45 +0700

    Thiết kế giao diện đăng nhập và đăng ký

---
+++ BDSKhanhHoa/ViewModels/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BDSKhanhHoa.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Vui lòng nhập Email hoặc Username")]
+        public string AccountName { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập mật khẩu")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
---
+++ BDSKhanhHoa/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+namespace BDSKhanhHoa.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Vui lòng nhập họ và tên")]
+        [Display(Name = "Họ và tên")]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập Email")]
+        [EmailAddress(ErrorMessage = "Định dạng Email không hợp lệ")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập số điện thoại")]
+        [RegularExpression(@"^0\d{9}$", ErrorMessage = "Số điện thoại phải có 10 số và bắt đầu bằng số 0")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập mật khẩu")]
+        [MinLength(6, ErrorMessage = "Mật khẩu phải từ 6 ký tự trở lên")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng xác nhận mật khẩu")]
+        [Compare("Password", ErrorMessage = "Mật khẩu xác nhận không trùng khớp")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
+        public string? Address { get; set; }
+        public string? CompanyName { get; set; }
+
+        public IFormFile? AvatarFile { get; set; }
+    }
+}
